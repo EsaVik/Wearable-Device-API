fd2d6c35a81433986d7e0aedd6650994ff19e849
Vikberg Esa <esa.vikberg@aalto.fi>
2023-06-08 10:24:01 +0300

    Fixed indentations and temperature calculation missing variable

--- Actuators/Peltier/I2c_slave_peltier.ino
+++ Actuators/Peltier/I2c_slave_peltier.ino
@@ -33,15 +33,22 @@ float sensorReadings[4];
 float temperatures[4];
 
 // For calculating temperature
-int VCC = 5; // Supply voltage
+// Thermistor properties
+// RT0: 10 000 Ω
+// T0: 25 C
+// B: 3977 K +- 0.75%
+
+float VCC = 5.00; // Supply voltage
 int R = 10000; // R = 10KΩ
+int RT0 = 10000; // RT0 = 10KΩ
+int B = 3977; // B = 3977
 float T0 = 298.15; // T0 in Kelvin
 float VR, RT, ln, TX;
 
 
 
 void setup() {
-	
+  
   // Initialize input and output pins
   pinMode(p1_pin1, OUTPUT);
   pinMode(p1_pin2, OUTPUT);
@@ -66,50 +73,51 @@ void loop() {
   // Peltier1
   // Heating side 1, cooling side 2
   if (direction1 == 0) {
-	  // If overheating, shut down pwm
-	  if (temperatures[0] > maximumTemperatureSide1 || temperatures[1] < minimumTemperatureSide2) {
-		analogWrite(p1_pin1, 0);
-		analogWrite(p1_pin2, 0);
-	  } else {
-		analogWrite(p1_pin1, intensity1);
-		analogWrite(p1_pin2, 0);
-	  }
+    // If overheating, shut down pwm
+    if (temperatures[0] > maximumTemperatureSide1 || temperatures[1] < minimumTemperatureSide2) {
+      analogWrite(p1_pin1, 0);
+      analogWrite(p1_pin2, 0);
+    } else {
+      analogWrite(p1_pin1, intensity1);
+      analogWrite(p1_pin2, 0);
+    }
   // Cooling side 1, heating side 2
   } else {
-	if (temperatures[0] < minimumTemperatureSide1 || temperatures[1] > maximumTemperatureSide2) {
-		analogWrite(p1_pin1, 0);
-		analogWrite(p1_pin2, 0);
-	  } else {
-		analogWrite(p1_pin1, 0);
-		analogWrite(p1_pin2, intensity1);
-	  }
+    if (temperatures[0] < minimumTemperatureSide1 || temperatures[1] > maximumTemperatureSide2) {
+      analogWrite(p1_pin1, 0);
+      analogWrite(p1_pin2, 0);
+    } else {
+      analogWrite(p1_pin1, 0);
+      analogWrite(p1_pin2, intensity1);
+    }
   }
   
   // Peltier2
   // Heating side 1, cooling side 2
   if (direction2 == 0) {
-	  if (temperatures[2] > maximumTemperatureSide1 || temperatures[3] < minimumTemperatureSide2) {
-		analogWrite(p2_pin1, 0);
-		analogWrite(p2_pin2, 0);
-	  } else {
-		analogWrite(p2_pin1, intensity1);
-		analogWrite(p2_pin2, 0);
-	  }
+    if (temperatures[2] > maximumTemperatureSide1 || temperatures[3] < minimumTemperatureSide2) {
+      analogWrite(p2_pin1, 0);
+      analogWrite(p2_pin2, 0);
+    } else {
+      analogWrite(p2_pin1, intensity1);
+      analogWrite(p2_pin2, 0);
+    }
   // Cooling side 1, heating side 2
   } else {
-	if (temperatures[2] < minimumTemperatureSide1 || temperatures[3] > maximumTemperatureSide2) {
-		analogWrite(p2_pin1, 0);
-		analogWrite(p2_pin2, 0);
-	  } else {
-		analogWrite(p2_pin1, 0);
-		analogWrite(p2_pin2, intensity1);
-	  }
+    if (temperatures[2] < minimumTemperatureSide1 || temperatures[3] > maximumTemperatureSide2) {
+      analogWrite(p2_pin1, 0);
+      analogWrite(p2_pin2, 0);
+    } else {
+      analogWrite(p2_pin1, 0);
+      analogWrite(p2_pin2, intensity1);
+    }
   }
 }
 
 // Handles incoming commands
 void readEvent(int count) {
-  command = Wire.read(); // All control messages start with command type
+  // All control messages start with command type
+  command = Wire.read();
   
   // Commands:
   // 0 - Get Type
@@ -119,17 +127,17 @@ void readEvent(int count) {
   if (command == 0) {
     Wire.write('p');
   } else if (command == 1) {
-	intensity1 = Wire.read();
-	direction1 = Wire.read();
-	intensity2 = Wire.read();
-	direction2 = Wire.read();
+    intensity1 = Wire.read();
+    direction1 = Wire.read();
+    intensity2 = Wire.read();
+    direction2 = Wire.read();
   } else if (command == 2) {
-	Wire.write(temperatures);
+    Wire.write(temperatures);
   } else if (command == 3) {
-	minimumTemperatureSide1 = Wire.read();
-	maximumTemperatureSide1 = Wire.read();
-	minimumTemperatureSide2 = Wire.read();
-	maximumTemperatureSide2 = Wire.read();
+    minimumTemperatureSide1 = Wire.read();
+    maximumTemperatureSide1 = Wire.read();
+    minimumTemperatureSide2 = Wire.read();
+    maximumTemperatureSide2 = Wire.read();
   }
 }
 
@@ -140,23 +148,18 @@ void calculateTemperatures() {
   sensorReadings[2] = analogRead(t3_pin);
   sensorReadings[3] = analogRead(t4_pin);
   
-  // Thermistor properties
-  // RT0: 10 000 Ω
-  // T0: 25 C
-  // B: 3977 K +- 0.75%
-  
   for (int i = 0; i < 4; i++) {
-	// Convert to voltage
-	// Supply voltage is 5V and ADC has values between 0-1023
-    sensorReadings[i] = (5.00 / 1023.00) * sensorReadings[i];
-	VR = VCC - VRT;
-	// Calculate resistance of RT+
-	RT = VRT / (VR / R);
-	
-	ln = log(RT / RT0);
-	TX = (1 / ((ln / 3977) + (1 / T0)));
-	
-	// Convert to celsius and store
-	temperatures[i] = TX - 273.15;
+    // Convert to voltage
+    // Supply voltage is 5V and ADC has values between 0-1023
+    sensorReadings[i] = (VCC / 1023.00) * sensorReadings[i];
+    VR = VCC - sensorReadings[i];
+    // Calculate resistance of RT+
+    RT = sensorReadings[i] / (VR / R);
+  
+    ln = log(RT / RT0);
+    TX = (1 / ((ln / B) + (1 / T0)));
+  
+    // Convert to celsius and store
+    temperatures[i] = TX - 273.15;
   }
 }
